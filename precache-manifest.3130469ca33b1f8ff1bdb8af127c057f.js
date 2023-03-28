ca489b23086342d4346a7b537aa9cb5ba3d9f6b1
Ritwik2008 <92855941+Ritwik2008@users.noreply.github.com>
2023-03-28 09:23:39 +0530

    Add files via upload

---
+++ precache-manifest.3130469ca33b1f8ff1bdb8af127c057f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e209c3681e79033ead9",
+    "url": "/static/css/main.0eb38f88.chunk.css"
+  },
+  {
+    "revision": "8e209c3681e79033ead9",
+    "url": "/static/js/main.82ec1fca.chunk.js"
+  },
+  {
+    "revision": "44379f9d8a742a556af9",
+    "url": "/static/js/runtime~main.1b973b83.js"
+  },
+  {
+    "revision": "bbb6249a1ebd933db337",
+    "url": "/static/js/2.129d0903.chunk.js"
+  },
+  {
+    "revision": "9e493117a3dfb5238748",
+    "url": "/static/js/3.f326d3da.chunk.js"
+  },
+  {
+    "revision": "16cc8158b2b7666900d23865a991c0d0",
+    "url": "/index.html"
+  }
+];
